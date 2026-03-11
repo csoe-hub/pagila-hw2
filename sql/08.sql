3e7ab83a172e6b9bb048fd41c1ab50382d5d721e
Carter Soe <csoe@students.pitzer.edu>
2026-03-11 15:04:21 -0700

    complete SQL homework

--- sql/08.sql
+++ sql/08.sql
@@ -5,3 +5,10 @@
  * HINT:
  * Use `unnest(special_features)` in a subquery.
  */
+SELECT title
+FROM film f
+WHERE rating = 'G'
+AND 'Trailers' IN (
+    SELECT unnest(f.special_features)
+)
+ORDER BY title;
--- sql/17.sql
+++ sql/17.sql
@@ -8,3 +8,23 @@
  * You might find the following stackoverflow answer useful for figuring out the syntax:
  * <https://stackoverflow.com/a/5700744>.
  */
+SELECT
+    rank,
+    title,
+    revenue,
+    SUM(revenue) OVER (ORDER BY revenue DESC) AS "total revenue"
+FROM (
+    SELECT
+        RANK() OVER (ORDER BY COALESCE(SUM(p.amount),0.00) DESC) AS rank,
+        f.title,
+        COALESCE(SUM(p.amount),0.00) AS revenue
+    FROM film f
+    LEFT JOIN inventory i
+        ON f.film_id = i.film_id
+    LEFT JOIN rental r
+        ON i.inventory_id = r.inventory_id
+    LEFT JOIN payment p
+        ON r.rental_id = p.rental_id
+    GROUP BY f.film_id, f.title
+) t
+ORDER BY revenue DESC, title;
